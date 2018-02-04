185d8d95072f9b78d2c117f2c32327db0082a499
Priyanka D Shah <prishah1194@gmail.com>
2018-02-03 23:51:20 -0500

    style

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,29 +1,94 @@
 import React from 'react'
-
-
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap'
 var headerStyle={
-    margin:"10px",
-    padding:"20px",
-   fontFamily:"Arial",
+    //margin:"10px",
+    //paddingLeft:"20px",
+    textAlign:"center",
+   fontFamily:"Times New Roman",
+   //paddingBottom:"10px"
   // borderRadius:"30px",   
    //boxShadow:"5px 5px 5px 5px #505359"
 }
+var ib={
+    display:"inline-block",
+    width:"710px",
+    padding:"20px"
+}
+
+
+
+
 export default function Home() {
     return (
+        <div style={{color:"#5d5d5e"}}>
+             <link href="../bootstrap/css/bootstrap.css" rel="stylesheet"></link>
+             <link href="../bootstrap/css/bootstrap-responsive.css" rel="stylesheet"></link>                   
+        
         <div>
-        <div>
-            <h1 style={headerStyle}>JavaScript</h1>
+            <h1 style={headerStyle}>Lorem Ipsum</h1>
         </div>
-        <div style={{margin:"20px"}}>
-            <h2>About</h2>
-            <h3>
-                This is Margaret Hamilton Interplanetary Academy of JavaScript
-            </h3>
-            <h2>History</h2>
-            <h3>
-                It was founded by Priyanka Shah &#9786;
-            </h3>
-            </div>
+        
+        <div style={{marginLeft:"20px", fontFamily:"Times New Roman"}}>
+        <div style={ib}>
+        <h2> About</h2>
+        <h3>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+            Aldus PageMaker including versions of Lorem Ipsum.
+        </h3>
+        </div>
+        <div style={ib}>
+        <Carousel style={{paddingLeft:"30px", paddingRight:"30px", width:"500px",height:"294px"}}>
+  <Carousel.Item>
+    <img width={1900} height={500} alt="900x500" src="https://intuitturbotax.files.wordpress.com/2010/12/books.jpg" />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={1900} height={500} alt="900x500" src="https://intuitturbotax.files.wordpress.com/2010/12/books.jpg" />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={1900} height={500} alt="900x500" src="https://intuitturbotax.files.wordpress.com/2010/12/books.jpg" />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+        </div>
+        <div style={{paddingLeft:"20px", paddingRight:"15px"}}>
+            <h2>Where does it come from?</h2>
+            <h3>       
+Contrary to popular belief, 
+Lorem Ipsum is not simply random text. 
+It has roots in a piece of classical Latin literature from 45 BC, 
+making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney 
+College in Virginia, looked up one of the more obscure Latin words, consectetur, 
+from a Lorem Ipsum passage, and going through the cites of the word in classical l
+iterature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 
+and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by 
+Cicero, written in 45 BC. This book is a treatise on the theory of ethics, 
+very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
+sit amet..", comes from a line in section 1.10.32.<br/>
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for 
+those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" 
+by Cicero are also reproduced in their exact original form, accompanied by English 
+versions from the 1914 translation by H. Rackham.<br/>
+    </h3>
+        </div>
+        
+        </div>
         </div>
     );
 }
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { Button, Nav, NavItem, NavDropdown, MenuItem,Navbar, Glyphicon } from 'react-bootstrap';
+
 
 var navStyle={
-    background:"#2e4160",
+    background:"#393e47",
     margin:"10px",
     padding:"10px",
     color:"white",
@@ -19,15 +21,25 @@ var buttonHome={
 }
 
 
-export default function Navbar(props) {
+export default function Navbar1(props) {
     return (
         <div>
+         
+{/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</link>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</link>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> */}
+{/*    
         <nav style={navStyle}>
             <span>
             <button style={buttonHome}>
             <NavLink to="/" style={{textDecoration: 'none'}}><h2> HOME </h2></NavLink>
             </button>
             </span>
+           
             <span>
                 <button style={buttonHome}>
             <NavLink to="/students" style={{textDecoration: 'none'}}><h2> STUDENTS</h2> </NavLink>
@@ -38,7 +50,44 @@ export default function Navbar(props) {
             <NavLink to="/campuses" style={{textDecoration: 'none'}}> <h2>CAMPUSES</h2> </NavLink>
             </button>
             </span>
-        </nav>
+    
+   
+        </nav> */}
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+            </link>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            </link>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            <Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <NavLink to="/" style={{textDecoration: 'none'}}>Campus Management Project</NavLink>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1}>
+      <NavLink to="/" style={{textDecoration: 'none'}} eventKey={1}>
+      <Glyphicon glyph="home" />
+        </NavLink>
+      </NavItem>
+
+      <NavItem eventKey={2} style={{color:"white"}}>
+      <NavLink to="/students" style={{textDecoration: 'none'}}>
+        Students
+        </NavLink>
+      </NavItem>
+    <NavItem eventKey={3}>
+    <NavLink to="/campuses" style={{textDecoration: 'none'}}>
+        Campuses
+        </NavLink>
+        </NavItem>
+        </Nav>
+  </Navbar.Collapse>
+</Navbar>
         </div>
     );
 }
